1043f29f42d7db98574eeb674b80b1d343ba2eea
AuliaDarni <114340110+AuliaDarni@users.noreply.github.com>
2022-10-30 19:37:57 +0700

    Add files via upload

---
+++ halaman-utama.php
@@ -0,0 +1,12 @@
+<?php
+include 'functions.php';
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p align="center" >SELAMAT DATANG REZKI AULIA DARNI SAPUTRI</p>
+</div>
+
+<?=template_footer()?>
